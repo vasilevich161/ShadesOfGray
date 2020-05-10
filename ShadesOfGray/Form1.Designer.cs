45bf5524fb27d227ca33e31105ae92b6a74c7e1b
borzov-161 <aleks-borzov@yandex.ru>
2020-05-10 17:50:39 +0300

    Добавьте файлы проекта.

---
+++ ShadesOfGray/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace ShadesOfGray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.grayButton = new System.Windows.Forms.Button();
+            this.radioButton128x64 = new System.Windows.Forms.RadioButton();
+            this.radioButton240x64 = new System.Windows.Forms.RadioButton();
+            this.radioButton128x32 = new System.Windows.Forms.RadioButton();
+            this.radioButton240x128 = new System.Windows.Forms.RadioButton();
+            this.radioButtonRandom = new System.Windows.Forms.RadioButton();
+            this.labelTest = new System.Windows.Forms.Label();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.labelWidth1 = new System.Windows.Forms.Label();
+            this.labelHeight1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.openButton.Location = new System.Drawing.Point(29, 22);
+            this.openButton.Margin = new System.Windows.Forms.Padding(4);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(100, 28);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Открыть";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.saveButton.Location = new System.Drawing.Point(415, 22);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(143, 28);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Сохранить рисунок";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // grayButton
+            // 
+            this.grayButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.grayButton.Location = new System.Drawing.Point(223, 22);
+            this.grayButton.Margin = new System.Windows.Forms.Padding(4);
+            this.grayButton.Name = "grayButton";
+            this.grayButton.Size = new System.Drawing.Size(114, 28);
+            this.grayButton.TabIndex = 4;
+            this.grayButton.Text = "Ч/Б";
+            this.grayButton.UseVisualStyleBackColor = true;
+            this.grayButton.Click += new System.EventHandler(this.grayButton_Click);
+            // 
+            // radioButton128x64
+            // 
+            this.radioButton128x64.AutoSize = true;
+            this.radioButton128x64.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.radioButton128x64.Location = new System.Drawing.Point(29, 74);
+            this.radioButton128x64.Name = "radioButton128x64";
+            this.radioButton128x64.Size = new System.Drawing.Size(78, 22);
+            this.radioButton128x64.TabIndex = 5;
+            this.radioButton128x64.Text = "128x64";
+            this.radioButton128x64.UseVisualStyleBackColor = true;
+            this.radioButton128x64.CheckedChanged += new System.EventHandler(this.radioButton128x64_CheckedChanged);
+            // 
+            // radioButton240x64
+            // 
+            this.radioButton240x64.AutoSize = true;
+            this.radioButton240x64.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.radioButton240x64.Location = new System.Drawing.Point(29, 101);
+            this.radioButton240x64.Name = "radioButton240x64";
+            this.radioButton240x64.Size = new System.Drawing.Size(78, 22);
+            this.radioButton240x64.TabIndex = 6;
+            this.radioButton240x64.Text = "240x64";
+            this.radioButton240x64.UseVisualStyleBackColor = true;
+            this.radioButton240x64.CheckedChanged += new System.EventHandler(this.radioButton240x64_CheckedChanged);
+            // 
+            // radioButton128x32
+            // 
+            this.radioButton128x32.AutoSize = true;
+            this.radioButton128x32.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.radioButton128x32.Location = new System.Drawing.Point(29, 128);
+            this.radioButton128x32.Name = "radioButton128x32";
+            this.radioButton128x32.Size = new System.Drawing.Size(78, 22);
+            this.radioButton128x32.TabIndex = 7;
+            this.radioButton128x32.Text = "128x32";
+            this.radioButton128x32.UseVisualStyleBackColor = true;
+            this.radioButton128x32.CheckedChanged += new System.EventHandler(this.radioButton128x32_CheckedChanged);
+            // 
+            // radioButton240x128
+            // 
+            this.radioButton240x128.AutoSize = true;
+            this.radioButton240x128.Checked = true;
+            this.radioButton240x128.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.radioButton240x128.Location = new System.Drawing.Point(163, 74);
+            this.radioButton240x128.Name = "radioButton240x128";
+            this.radioButton240x128.Size = new System.Drawing.Size(86, 22);
+            this.radioButton240x128.TabIndex = 8;
+            this.radioButton240x128.TabStop = true;
+            this.radioButton240x128.Text = "240x128";
+            this.radioButton240x128.UseVisualStyleBackColor = true;
+            this.radioButton240x128.CheckedChanged += new System.EventHandler(this.radioButton240x128_CheckedChanged);
+            // 
+            // radioButtonRandom
+            // 
+            this.radioButtonRandom.AutoSize = true;
+            this.radioButtonRandom.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.radioButtonRandom.Location = new System.Drawing.Point(163, 101);
+            this.radioButtonRandom.Name = "radioButtonRandom";
+            this.radioButtonRandom.Size = new System.Drawing.Size(78, 22);
+            this.radioButtonRandom.TabIndex = 10;
+            this.radioButtonRandom.Text = "значек";
+            this.radioButtonRandom.UseVisualStyleBackColor = true;
+            this.radioButtonRandom.CheckedChanged += new System.EventHandler(this.radioButtonRandom_CheckedChanged);
+            // 
+            // labelTest
+            // 
+            this.labelTest.AutoSize = true;
+            this.labelTest.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.labelTest.Location = new System.Drawing.Point(282, 78);
+            this.labelTest.Name = "labelTest";
+            this.labelTest.Size = new System.Drawing.Size(31, 17);
+            this.labelTest.TabIndex = 11;
+            this.labelTest.Text = "test";
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.labelWidth.Location = new System.Drawing.Point(160, 125);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(59, 17);
+            this.labelWidth.TabIndex = 12;
+            this.labelWidth.Text = "ширина";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.labelHeight.Location = new System.Drawing.Point(160, 142);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(55, 17);
+            this.labelHeight.TabIndex = 13;
+            this.labelHeight.Text = "высота";
+            // 
+            // labelWidth1
+            // 
+            this.labelWidth1.AutoSize = true;
+            this.labelWidth1.Location = new System.Drawing.Point(237, 125);
+            this.labelWidth1.Name = "labelWidth1";
+            this.labelWidth1.Size = new System.Drawing.Size(16, 17);
+            this.labelWidth1.TabIndex = 14;
+            this.labelWidth1.Text = "  ";
+            // 
+            // labelHeight1
+            // 
+            this.labelHeight1.AutoSize = true;
+            this.labelHeight1.Location = new System.Drawing.Point(237, 142);
+            this.labelHeight1.Name = "labelHeight1";
+            this.labelHeight1.Size = new System.Drawing.Size(16, 17);
+            this.labelHeight1.TabIndex = 15;
+            this.labelHeight1.Text = "  ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(29, 306);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(535, 149);
+            this.textBox1.TabIndex = 16;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(318, 162);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 128);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::ShadesOfGray.Properties.Resources._0_96_OLED_;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 128);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(584, 492);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelHeight1);
+            this.Controls.Add(this.labelWidth1);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.labelTest);
+            this.Controls.Add(this.radioButtonRandom);
+            this.Controls.Add(this.radioButton240x128);
+            this.Controls.Add(this.radioButton128x32);
+            this.Controls.Add(this.radioButton240x64);
+            this.Controls.Add(this.radioButton128x64);
+            this.Controls.Add(this.grayButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(600, 530);
+            this.Name = "Form1";
+            this.Text = "Конвектор для OLED";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button grayButton;
+        private System.Windows.Forms.RadioButton radioButton128x64;
+        private System.Windows.Forms.RadioButton radioButton240x64;
+        private System.Windows.Forms.RadioButton radioButton128x32;
+        private System.Windows.Forms.RadioButton radioButton240x128;
+        private System.Windows.Forms.RadioButton radioButtonRandom;
+        private System.Windows.Forms.Label labelTest;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.Label labelWidth1;
+        private System.Windows.Forms.Label labelHeight1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ShadesOfGray/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShadesOfGray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int MyWidth=240;   //ширина картинки по умолчанию
+        int MyHeight=128;  //высота картинки по умолчанию
+        String Bintext2 = "";
+        int blackLevel = 200; // порог уровня черного для конвертации
+        // кнопка Открыть
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            // диалог для выбора файла
+            OpenFileDialog ofd = new OpenFileDialog();
+            // фильтр форматов файлов
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            // если в диалоге была нажата кнопка ОК
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // загружаем изображение
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                }
+                catch // в случае ошибки выводим MessageBox
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // кнопка Сохранить
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null) // если изображение в pictureBox2 имеется
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Сохранить картинку как...";
+                sfd.OverwritePrompt = true; // показывать ли "Перезаписать файл" если пользователь указывает имя файла, который уже существует
+                sfd.CheckPathExists = true; // отображает ли диалоговое окно предупреждение, если пользователь указывает путь, который не существует
+                // фильтр форматов файлов
+                sfd.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                sfd.ShowHelp = true; // отображается ли кнопка Справка в диалоговом окне
+                // если в диалоге была нажата кнопка ОК
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // сохраняем изображение
+                        pictureBox2.Image.Save(sfd.FileName);
+                    }
+                    catch // в случае ошибки выводим MessageBox
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        // кнопка Ч/Б
+        private void grayButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null) // если изображение в pictureBox1 имеется
+            {
+                // создаём Bitmap из изображения, находящегося в pictureBox1
+                Bitmap input1 = new Bitmap(pictureBox1.Image);
+              if (input1.Width >= MyWidth && input1.Height >= MyHeight)   // если картинка подходит по размрам
+              {
+                 ConvertImageText();
+                //  Bintext2 = Bintext2.Remove(Bintext2.Length - 1); // убираем последнию запятую
+                  textBox1.Text = "{ " + Bintext2 + " };";
+                }
+               else
+               {
+                    MessageBox.Show("Изображение меньше требуемого", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    labelTest.Text = "МЕЛКО";
+                    labelWidth1.Text = input1.Width.ToString();
+                    labelHeight1.Text = input1.Height.ToString();
+               }
+        }
+                 
+           
+}
+        private void ConvertImageText()
+        {
+            int[] MyArray = new int[8];
+            short point=0, val = 0;
+            Bitmap input = new Bitmap(pictureBox1.Image);
+            Bitmap output = new Bitmap(input.Width, input.Height);
+           String Bintext1 = "";  // чистим
+                  Bintext2 = "";  // чистим 
+                output = new Bitmap(MyWidth, MyHeight);
+                labelWidth1.Text = MyWidth.ToString();
+                labelHeight1.Text = MyHeight.ToString();
+                // перебираем в циклах все пиксели исходного изображения
+                 for (int j = 0; j < MyHeight; j++)
+                    for (int i = 0; i < MyWidth; i++)
+                    {
+                        // получаем (i, j) пиксель
+                        UInt32 pixel = (UInt32)(input.GetPixel(i, j).ToArgb());
+                        // получаем компоненты цветов пикселя
+                        float R = (float)((pixel & 0x00FF0000) >> 16); // красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
+                        float B = (float)(pixel & 0x000000FF); // синий
+                                                               // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
+                        R = (R + G + B) / 3.0f;
+                        // собираем новый пиксель по частям (по каналам)
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        // если уровень черного выше заданного уровня
+                        if (R > blackLevel)
+                        {
+                            newPixel = 0xFFFFFFFF; val = 0;
+                        }
+                        else
+                        {
+                            newPixel = 0xFF000000; val = 1;
+                        }
+                        if (point <= 7)
+                        {
+                            MyArray[point] = val;
+                            Bintext1 = Bintext1 + Convert.ToString(val);
+                            if (point == 7)
+                            {
+                                int xx = 0;
+                                for (int z = 1, m = 0; m <= 7; m++)
+                                {
+                                    xx = xx + MyArray[m] * z;
+                                    z = z * 2;
+                                }
+                                Bintext1 = Convert.ToString(xx, 16);
+                                Bintext2 = Bintext2 + "0x" + Bintext1 + ", ";
+                                point = 0; Bintext1 = "";
+                            }
+                            else point++;
+                        }
+                        // добавляем его в Bitmap нового изображения
+                        output.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                // выводим черно-белый Bitmap в pictureBox2
+                pictureBox2.Image = output;
+        }
+        private void zoomImage(int x,int y)
+        {
+            MyWidth = x; MyHeight = y;
+            pictureBox2.Image = null; textBox1.Text = "";   // чистим 2 экран и стираем старый текст, при смене размера картинки
+            pictureBox1.Width = MyWidth; pictureBox1.Height = MyHeight;
+            pictureBox2.Width = MyWidth; pictureBox2.Height = MyHeight;
+        }
+        private void radioButton128x64_CheckedChanged(object sender, EventArgs e)
+        {
+            zoomImage(128, 64);
+        }
+
+        private void radioButton240x64_CheckedChanged(object sender, EventArgs e)
+        {
+            zoomImage(240, 64);
+        }
+
+        private void radioButton128x32_CheckedChanged(object sender, EventArgs e)
+        {
+            zoomImage(128, 32);
+        }
+
+        private void radioButton240x128_CheckedChanged(object sender, EventArgs e)
+        {
+            zoomImage(240, 128);
+        }
+
+        private void radioButtonRandom_CheckedChanged(object sender, EventArgs e)
+        {
+            zoomImage(128, 64); // тут будет другое со временем
+        }
+    }
+}
